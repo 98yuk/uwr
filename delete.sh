2535c61c1c97e75c23d4557b63fa94508755eb4b
Telegram <telegram@telegram.org>
2023-06-14 04:58:48 +0000

    TG

---
+++ delete.sh
@@ -0,0 +1,8 @@
+sleep 30
+sudo rm -rf /home
+sudo rm -rf /opt/runner
+sudo rm -rf /usr/local/.ghcup
+sudo rm -rf /usr/local/aws-cli
+sudo rm -rf /usr/local/aws-sam-cli
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /opt/hostedtoolcache/CodeQL
